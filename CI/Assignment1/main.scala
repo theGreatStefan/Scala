48a2365c03d1f01609163744f071a3f0469c330e
theGreatStefan <stefanvd44@gmail.com>
2020-02-29 11:59:50 +0200

    Started adding experiments

--- CI/Assignment1/main.scala
+++ CI/Assignment1/main.scala
@@ -3,6 +3,6 @@ object main extends App {
     for (i <- 0 to 0) {
         var swarm1 = new swarm(30, 30, 0.7, 0.7, 0.9)
         swarm1.runSwarm(5000)
-        println(swarm1.globalBest())
+        swarm1.printTests()
     }
 }
--- CI/Assignment1/swarm.scala
+++ CI/Assignment1/swarm.scala
@@ -11,6 +11,8 @@ class swarm(iswarm_size:Int, iconstraint_size:Int, ic1:Double, ic2:Double, iw:Do
     var gbest_pos:Array[Double] = Array.fill(constraint_size){-0.5 + r.nextDouble()*1}
     var gbest_score:Double = f25(gbest_pos)
     var pswarm:Array[particle] = Array()
+    var avg_eucl_d_time:Array[Double] = Array()
+    var gbest_score_time:Array[Double] = Array()
     
     for (i <- 0 to swarm_size-1) {
         var particle_pos:Array[Double] = Array.fill(constraint_size){-0.5 + r.nextDouble()*1}
@@ -44,6 +46,8 @@ class swarm(iswarm_size:Int, iconstraint_size:Int, ic1:Double, ic2:Double, iw:Do
                     gbest_pos = pbest_pos
                 }
             }
+            gbest_score_time = gbest_score_time :+ gbest_score
+            avg_eucl_d_time = avg_eucl_d_time :+ avgEuclidianDistance()
         }
     }
 
@@ -51,10 +55,35 @@ class swarm(iswarm_size:Int, iconstraint_size:Int, ic1:Double, ic2:Double, iw:Do
         gbest_score
     }
 
+    def avgParticle(): Array[Double] = {
+        var avg_pos:Array[Double] = pswarm(0).pos
+        for (i <- 1 to swarm_size-1) {
+            avg_pos = avg_pos.zip(pswarm(i).pos).map{case (a,b) => a+b}
+        }
+        avg_pos = avg_pos.map(el => el/swarm_size)
+        avg_pos
+    }
+
+    def avgEuclidianDistance():Double = {
+        var avg_pos:Array[Double] = avgParticle()
+        var avg_eucl_d:Double = Math.sqrt((pswarm(0).pos.zip(avg_pos).map{case (a,b) => Math.pow((a-b), 2)}).sum)
+        for (i <- 1 to swarm_size-1) {
+            avg_eucl_d = avg_eucl_d + Math.sqrt((pswarm(i).pos.zip(avg_pos).map{case (a,b) => Math.pow((a-b), 2)}).sum)
+        }
+        (avg_eucl_d/swarm_size)
+    }
+
+    def printTests(): Unit = {
+        println("Average Euclidian distance over time:")
+        (for (i <- 0 to avg_eucl_d_time.length-1) yield (println(avg_eucl_d_time(i))))
+        println("\nAverage global best score over time:")
+        (for (i <- 0 to gbest_score_time.length-1) yield (println(gbest_score_time(i))))
+    }
+
     override def toString(): String = {
-        "---------------------"+
-        "\nGlobal best position: "+ (for (i <- 0 to constraint_size-1) yield (gbest_pos(i))) +
-        "\nGlobal best score: "+ gbest_score+
-        "\n---------------------"
+        "Average Euclidian distance over time:\n"+
+        (for (i <- 0 to avg_eucl_d_time.length-1) yield ((avg_eucl_d_time(i)+"\n")))+
+        "\nAverage global best score over time:\n"+
+        (for (i <- 0 to gbest_score_time.length-1) yield ((gbest_score_time(i)+"\n")))
     }
 }
